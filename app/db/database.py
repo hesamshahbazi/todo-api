8add2377c24e213c98875c10c138e39490275b84
Hesam-shahbazi <hesam.shahbazi_dastjerdi@smail.th-koeln.de>
2026-02-17 17:53:15 +0100

    Add docker-compose and ignore db/cache files

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = "sqlite:///./data/todo.db"
 
 engine = create_engine(
     DATABASE_URL,
